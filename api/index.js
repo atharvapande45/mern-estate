9abd0c6790c660db5e0f48194b09abe0299e3a1e
atharvapande45 <atharva.pande20@pccoepune.org>
2024-03-26 11:14:05 +0530

    added pre installation

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -14,6 +15,8 @@ mongoose
         console.log(err);
     });
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 
@@ -23,6 +26,11 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
