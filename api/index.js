9ec15d8bccbad8e895d88bda8a6f4347c7e29d6c
atharvapande45 <atharva.pande20@pccoepune.org>
2024-03-26 11:31:15 +0530

    Revert "added pre installation"
    This reverts commit 9abd0c6790c660db5e0f48194b09abe0299e3a1e.

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import path from "path";
 dotenv.config();
 
 mongoose
@@ -15,8 +14,6 @@ mongoose
         console.log(err);
     });
 
-const __dirname = path.resolve();
-
 const app = express();
 app.use(express.json());
 
@@ -26,11 +23,6 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-
-app.use(express.static(path.join(__dirname, "/client/dist")));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
