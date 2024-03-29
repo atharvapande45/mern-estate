fb322d2d5a2d488991e981180ecb26b626421d22
atharvapande45 <atharva.pande20@pccoepune.org>
2024-03-29 16:58:21 +0530

    completed show user listing functionality

--- api/index.js
+++ api/index.js
@@ -30,12 +30,12 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
-// app.use(express.static(path.join(__dirname, "/client/dist")));
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
