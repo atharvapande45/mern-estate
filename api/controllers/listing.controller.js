0c579036352c1bff20cf78cbd015cb714fa11c5a
atharvapande45 <atharva.pande20@pccoepune.org>
2024-04-04 00:26:27 +0530

    created update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
@@ -10,21 +11,35 @@ export const createListing = async (req, res, next) => {
 };
 
 export const deleteListing = async (req, res, next) => {
-    const listing = await Listing.findById(req.params.id);
-
-    if (!listing) {
-        return next(errorHandler(404, "Listing not found!"));
-    }
-
-    if (req.user.id !== listing.userRef) {
-        return next(
-            errorHandler(404, "You can only delete your own listings !")
-        );
+    try {
+        const listing = await Listing.findById(req.params.id);
+    } catch (error) {
+        next(error);
     }
 
     try {
         await Listing.findByIdAndDelete(req.params.id);
         res.status(200).json("Listing has been deleted !");
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+    } catch (error) {
+        next(error);
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        res.status(200).json(updatedListing);
     } catch (error) {
         next(error);
     }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,7 @@
 import {
     createListing,
     deleteListing,
+    updateListing,
 } from "../controllers/listing.controller.js";
 import express from "express";
 import { verifyToken } from "./../utils/verifyUser.js";
@@ -9,4 +10,6 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+
 export default router;
