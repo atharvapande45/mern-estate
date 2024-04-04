3eeaaf9f3f37f4dc007e669a415a347f967f0443
atharvapande45 <atharva.pande20@pccoepune.org>
2024-04-04 16:08:03 +0530

    Added image slider to the listing page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -281,7 +281,10 @@ export default function Profile() {
                                     className="h-16 w-16 object-contain"
                                 />
                             </Link>
-                            <Link className="text-slate-700 font-semibold hover:underline truncate flex-1">
+                            <Link
+                                className="text-slate-700 font-semibold hover:underline truncate flex-1"
+                                to={`/listing/${listing._id}`}
+                            >
                                 <p>{listing.name}</p>
                             </Link>
                             <div className="flex flex-col item-center">
